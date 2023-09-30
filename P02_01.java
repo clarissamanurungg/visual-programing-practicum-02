692bacd71229a1efdc71896151913565d7565aea
clarissamanurungg <clarissamanurung0807@gmail.com>
2023-09-30 21:26:31 +0700

    12S23049 Clarissa Manurung
    Telah menyelesaikan tugas

--- P02_01.java
+++ P02_01.java
@@ -1,1 +1,25 @@
-// NIM - Your Name
+// 12S23049 - Clarissa Manurung
+
+import java.util.*;
+import java.lang.Math;
+
+class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String deskripsiTugas;
+        String kodeMataKuliah;
+        String namaMataKuliah;
+        String pengampu;
+        String deadline;
+        String formatPenamaanTugas;
+
+        deskripsiTugas = input.nextLine();
+        kodeMataKuliah = input.nextLine();
+        namaMataKuliah = input.nextLine();
+        pengampu = input.nextLine();
+        deadline = input.nextLine();
+        formatPenamaanTugas = input.nextLine();
+        System.out.println(deskripsiTugas + "|" + kodeMataKuliah + "|" + namaMataKuliah + "|" + pengampu + "|" + deadline + "|" + formatPenamaanTugas);
+    }
+}
