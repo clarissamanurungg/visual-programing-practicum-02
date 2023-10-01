e2439fb238132b42ee38226d43732ea4b225be79
clarissamanurungg <clarissamanurung0807@gmail.com>
2023-10-01 13:10:55 +0700

    12S23049_Clarissa Manurung
    Maaf salah kirim tugas yang pertama

--- P02_01.java
+++ P02_01.java
@@ -7,19 +7,36 @@ class Program {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String deskripsiTugas;
-        String kodeMataKuliah;
-        String namaMataKuliah;
-        String pengampu;
-        String deadline;
-        String formatPenamaanTugas;
+        String namaTugas, kode, matkul, namaDosen, deadline, format, status, hasil1, hasil2, hasil3;
+        int tingkatkesulitan, harideadline;
+        double prioritas;
 
-        deskripsiTugas = input.nextLine();
-        kodeMataKuliah = input.nextLine();
-        namaMataKuliah = input.nextLine();
-        pengampu = input.nextLine();
+        namaTugas = input.nextLine();
+        kode = input.nextLine();
+        matkul = input.nextLine();
+        namaDosen = input.nextLine();
         deadline = input.nextLine();
-        formatPenamaanTugas = input.nextLine();
-        System.out.println(deskripsiTugas + "|" + kodeMataKuliah + "|" + namaMataKuliah + "|" + pengampu + "|" + deadline + "|" + formatPenamaanTugas);
+        format = input.nextLine();
+        tingkatkesulitan = input.nextInt();
+        harideadline = input.nextInt();
+        status = input.nextLine();
+        prioritas = tingkatkesulitan * (1.0 / harideadline);
+        System.out.println("Prioritas :" + toFixed(prioritas,2));
+        if (prioritas > 3) {
+            hasil1 = "Penting! Anda harus mengerjakan tugas ini segera.";
+        } else {
+            if (prioritas >= 1.5 && prioritas <= 3) {
+                hasil1 = "Tugas ini memiliki prioritas menengah.";
+            } else {
+                if (prioritas < 1.5) {
+                    hasil1 = "Tugas ini relatif ringan, namun jangan tunda terlalu lama.";
+                }
+            }
+        }
+        System.out.println(namaTugas + "|" + kode + "|" + matkul + "|" + namaDosen + "|" + deadline + "|" + format + "|" + hasil1);
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
     }
 }
